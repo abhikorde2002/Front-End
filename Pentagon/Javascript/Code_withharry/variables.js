46baf00d316f6bb96fca195143b1e858e2fa7318
Abhishek korde <abhishekkorde44@gmail.comm>
2023-02-26 22:23:08 +0530

    Javascript

---
+++ Pentagon/Javascript/Code_withharry/variables.js
@@ -0,0 +1,15 @@
+//Javascript variable
+var num=10;
+var num1=20;
+console.log(num+num1);
+
+//Data types in js
+var num=33;
+var num1=23;
+
+//string
+var str="Abhi"
+var strr2="this is a string"
+
+//objects
+var marks={ravi:34, shubham:23,harry:34.3}
