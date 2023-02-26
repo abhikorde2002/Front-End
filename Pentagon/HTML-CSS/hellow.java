6d73a54e161645978ca93c2fb2113b79d9161da4
Abhishek korde <abhishekkorde44@gmail.comm>
2023-02-26 13:17:19 +0530

    Code added

---
+++ Pentagon/HTML-CSS/hellow.java
@@ -0,0 +1,7 @@
+class hellow{
+    public static void main(String[] args) {
+        String d="abhi";
+        String a=new String("abhi");
+        System.out.println("abhi"=="abhi");
+     }
+}
---
+++ Pentagon/jack.java
@@ -0,0 +1,9 @@
+package Pentagon;
+
+public class jack {
+    public static void main(String[] args) {
+        String d="SITHA";
+        String a="RAMA";
+        System.out.println(d.charAt(0)>a.charAt(0));
+    }
+}
